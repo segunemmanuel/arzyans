b378d3e4e51dfa009109639eebaa290398995204
segun <segunemmanuel46@gmail.com>
2021-09-15 20:57:15 -0700

    Update index.blade.php

--- resources/views/admin/slider/index.blade.php
+++ resources/views/admin/slider/index.blade.php
@@ -7,8 +7,15 @@
   <div class="py-12">
   <div class="container">
   <div class="row">
-  
-  <div class="col-md-8">
+
+
+    <h2>Home slider</h2>
+    <br>
+    <a href=""> <button class="btn btn-info">Add slider</button></a>
+  <br>
+
+
+  <div class="col-md-12">
   <div class="card">
   @if(session('success'))
   <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -29,31 +36,25 @@
   <tr>
   <th scope="col">S/N</th>
   <th scope="col">Slider Title</th>
-  <th scope="col">Slider image</th>
-  <th scope="col">Created at</th>
+  <th scope="col">Slider desc</th>
+  <th scope="col">Slider Image</th>
   <th scope="col">Action</th>
   
   </tr> 
   </thead>
   <tbody>
-    <!-- @php($i=1) -->
   
   
-  @foreach($brands as $brand)
+  @foreach($sliders as $slider)
   <tr>
     <!-- To make the pagination align correctyly -->
-    <th scope ="row"> {{ $brands->firstItem()+ $loop->index}}</th>
-  <td>{{ $brand->brand_name }}</td>
-  <td><img src="{{asset($brand->brand_image)}}" width="90px" height="60px" alt=""></td>
-  <td>
-  @if($brand->created_at==NULL)
-  <span class="text-danger">No date set </span>
-  @else
-  {{ Carbon\Carbon::parse($brand->created_at)->diffForHumans() }}
-  @endif
-  </td>
-  <td><a href="{{url('brand/edit/'. $brand->id)}}" class="btn btn-info">Edit</a> 
-  <a href="{{url('brand/delete/'.$brand->id)}}" onclick="return confirm('Are you sure to delete')" class="btn btn-danger">Delete</a>
+    <th scope ="row"> {{ $sliders->firstItem()+ $loop->index}}</th>
+  <td>{{ $slider->title }}</td>
+  <td>{{$slider->desc}}</td>
+  <td><img src="{{asset($slider->image)}}" width="90px" height="60px" alt=""></td>
+
+  <td><a href="{{url('brand/edit/'.$slider->id)}}" class="btn btn-info">Edit</a> 
+  <a href="{{url('brand/delete/'.$slider->id)}}" onclick="return confirm('Are you sure to delete')" class="btn btn-danger">Delete</a>
   
   </td> 
    </tr>
@@ -62,45 +63,11 @@
   </tbody>
   </table>
 
-  <nav aria-label="Page navigation">
-    {{ $brands->links()}}
-  
-  </nav>
   
   </div>
   </div>
   
-  <div class="col-md-4">
-  <div class="card">
-  <div class="card-header"> Add brand </div>
-  
-  <div class="card-body">
-  
-  <form action="{{ route('store.brand')}}" method="POST" enctype="multipart/form-data" >
-  @csrf
-    <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label">Brand name </label>
-  
-      <input type="text" name="brand_name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-      @error('brand_name')
-      <span class="text-danger"> {{$message}}</span>
-      @enderror
 
-      <label for="exampleInputEmail1" class="form-label">Brand image </label>
-      <input type="file" name="brand_image" class="form-control" id="">
-  
-  @error('brand_image')
-  <span class="text-danger"> {{$message}}</span>
-  @enderror
-  
-  
-    </div>
-      <button type="submit" class="btn btn-primary">Add brand</button>
-  </form>
-  </div>
-  
-  </div>
-  </div>
   </div>
   
   </div>
